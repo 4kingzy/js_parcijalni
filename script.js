6e4a217db91f09f6d6cf68c6449ea28a56b8572a
4kingzy <int.kingzy@gmail.com>
2023-02-27 20:17:50 +0100

    comit second

---
+++ script.js
@@ -0,0 +1,9 @@
+import { attendees } from "./data.js";
+const b = "hello";
+console.log(b);
+
+const inputEl = document.querySelector(".input");
+
+inputEl.addEventListener("keyup", (event) => {
+    return console.log(event);
+})
